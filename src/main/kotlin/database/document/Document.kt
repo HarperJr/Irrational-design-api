bfa90305967c8552d666a4fb756b57e3ff9618f7
Nikita Sychev <ns@digipeople.ru>
2019-05-26 11:34:06 +0300

    #Server
    - Hot fix.

--- src/main/kotlin/database/document/Document.kt
+++ src/main/kotlin/database/document/Document.kt
@@ -2,8 +2,9 @@ package database.document
 
 import org.bson.codecs.pojo.annotations.BsonId
 import org.litote.kmongo.Id
+import org.litote.kmongo.id.ObjectIdGenerator
 import org.litote.kmongo.newId
 
 abstract class Document<T> {
-    @BsonId val id: Id<T> = newId()
+    @BsonId val id: Id<T> = ObjectIdGenerator.generateNewId()
 }
